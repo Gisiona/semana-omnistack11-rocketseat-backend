3f2224ae502647222ccc5fc010988f72fca80b54
gisiona <gil-real@hotmail.com>
2020-03-28 23:57:44 -0300

    finalizado as funcionalidade do front-end parte web

--- frontend-bethehero/src/pages/incident/index.js
+++ frontend-bethehero/src/pages/incident/index.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 import { FiArrowLeft, FiTrash2 } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
-
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 export default function Incident(){
+
+    const [title , setTitle] =useState('');
+    const [description , setDescription] = useState('');
+    const [value , setValue] = useState('');
+    const ongId = localStorage.getItem('ongId');
+
+    const history = useHistory();
+
+    async function handleCriarIncident(e){
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        };
+
+        console.log(title, description, value);
+
+        try {
+
+            api.post('http://localhost:5000/incidents', data, {
+                headers: {
+                    Authorization: ongId,
+                }
+            }).then(response => {
+                console.log(response.data.data);
+            });
+
+            alert('Incidente cadastrado com sucesso');
+
+            history.push('/profile');
+        } catch (e) {
+            alert('Erro ao criar incidente. Tente novamente mais tarde.');
+        }
+    }
+
     return (
         <div className='incident-container'>
             <div className='content'>
@@ -21,10 +58,19 @@ export default function Incident(){
                     </Link>
                 </section>
 
-                <form>
-                    <input placeholder='Título do caso' />
-                    <textarea placeholder='Descrição' />
-                    <input placeholder='Valor em reais' />
+                <form onSubmit={handleCriarIncident }>
+                    <input placeholder='Título do caso' 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea placeholder='Descrição' 
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input placeholder='Valor em reais'
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <button className='button' type='submit'>Cadastrar</button>
                 </form>
